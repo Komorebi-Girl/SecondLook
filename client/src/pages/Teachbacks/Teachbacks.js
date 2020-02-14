091af5001079c1a4552d1743c9101a6593958d83
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2020-02-14 14:01:55 -0500

    created form that adds TBs  to database

--- client/src/pages/Teachbacks/Teachbacks.js
+++ client/src/pages/Teachbacks/Teachbacks.js
@@ -5,12 +5,13 @@ import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, FormBtn } from "../../components/Form";
+import Dropdown from "../../components/Form/Dropdown";
 
 class Teachbacks extends Component {
   // Setting our component's initial state
   state = {
     teachbacks: [],
-    value: "Select One",
+    value: "finalResult",
     candidateName: "",
     role: "",
     university: "",
@@ -65,40 +66,37 @@ class Teachbacks extends Component {
     });
   };
 
-  handleDropdownChange = event => {
-    if (
-      event.target.name !== "finalResult" &&
-      this.state.reviewedBy === "Nobody"
-    ) {
-      // Case 1: The value is selected from one of the category dropdowns and there's no reviewer
+  /* This function will take the scores selected via the category dropdowns & populate them
+   to the correct array based on who's doing the selecting: the submitter or reviewer */
+  updateScores = (value, reviewedBy, index) => {
+    // If no one's been assigned to review the TB, save the scores to the submitter array
+    if (reviewedBy === "Nobody") {
       var savedScores = [...this.state.submitterScores];
-      savedScores.push(event.target.value);
+      savedScores[index] = value;
       this.setState({ submitterScores: savedScores });
-    } else if (
-      event.target.name !== "finalResult" &&
-      this.state.reviewedBy !== "Nobody"
-    ) {
-      // Case 2: The value is selected from one of the category dropdowns and there's a reviewer
+    } else {
+      // If someone has been assigned, save the scores to the reviewer array
       savedScores = [...this.state.reviewerScores];
-      savedScores.push(event.target.value);
+      savedScores[index] = value;
       this.setState({ reviewerScores: savedScores });
-    } else if (
-      event.target.name === "finalResult" &&
-      this.state.reviewedBy === "Nobody"
-    ) {
-      // Case 3: The value is selected from the finalResult dropdown and there's no reviewer
-      this.setState({ submitterResult: event.target.value });
-    } else if (
-      event.target.name === "finalResult" &&
-      this.state.reviewedBy !== "Nobody"
-    ) {
-      // Case 4: The value is selected from the finalResult dropdown and there's a reviewer
+    }
+  };
+
+  updateFinalResult = event => {
+    // If no one's been assigned to review the TB, save the final result to submitterResult
+    if (this.state.reviewedBy === "Nobody") {
+      this.setState({
+        value: event.target.value,
+        submitterResult: event.target.value
+      });
+    } else {
+      // If someone has been assigned, save the final result to reviewerResult
       this.setState({ reviewerResult: event.target.value });
     }
   };
 
-  // When the form is submitted, use the API.saveTeachback method to save the teachback data
-  // Then reload teachbacks from the database
+  /* When the form is submitted, use the API.saveTeachback method to save the teachback data
+ Then reload teachbacks from the database */
   handleFormSubmit = event => {
     event.preventDefault();
     if (this.validateAllValues(this.state)) {
@@ -107,11 +105,14 @@ class Teachbacks extends Component {
         role: this.state.role,
         university: this.state.university,
         programType: this.state.programType,
+        reviewedBy: this.state.reviewedBy,
         submittedBy: this.state.submittedBy,
         zoomLink: this.state.zoomLink,
         cohortStartDate: this.state.cohortStartDate,
         submitterScores: this.state.submitterScores,
-        submitterResult: this.state.submitterResult
+        reviewerScores: this.state.reviewerScores,
+        submitterResult: this.state.submitterResult,
+        reviewerResult: this.state.reviewerResult
       })
         .then(res => this.loadTeachbacks())
         .catch(err => console.log(err));
@@ -119,9 +120,12 @@ class Teachbacks extends Component {
   };
 
   validateAllValues = obj => {
+    // Grab all of the values saved to this.state in the form of an array
     const valuesArray = Object.values(obj);
+    // Loop through the above array create a new array based on whether each value is true (truthy) or false (falsey)
     const booleanArray = valuesArray.map(val => Boolean(val));
-    booleanArray.every(bool => bool === true);
+    // Use "every" method to test if every property in this.state indeed has a value
+    return booleanArray.every(bool => bool === true);
   };
 
   render() {
@@ -133,6 +137,7 @@ class Teachbacks extends Component {
               <h1>Submit a Teachback!</h1>
             </Jumbotron>
             <form>
+              {/* Input boxes for the data that must be filled-in*/}
               <Input
                 value={this.state.candidateName}
                 onChange={this.handleInputChange}
@@ -175,29 +180,69 @@ class Teachbacks extends Component {
                 name="cohortStartDate"
                 placeholder="Cohort Start Date (required)"
               />
+              {/* Dropboxes for the data that must be selected */}
+              <Dropdown
+                category="Positivity"
+                index={0}
+                updateScores={this.updateScores}
+                reviewedBy={this.state.reviewedBy}
+                isSubmitted={Boolean(this.state.submittedBy)}
+              />
+              <Dropdown
+                category="Investment"
+                index={1}
+                updateScores={this.updateScores}
+                reviewedBy={this.state.reviewedBy}
+              />
+              <Dropdown
+                category="Pace"
+                index={2}
+                updateScores={this.updateScores}
+                reviewedBy={this.state.reviewedBy}
+              />
+              <Dropdown
+                category="Clarity"
+                index={3}
+                updateScores={this.updateScores}
+                reviewedBy={this.state.reviewedBy}
+              />
+              <Dropdown
+                category="Knowledge"
+                index={4}
+                updateScores={this.updateScores}
+                reviewedBy={this.state.reviewedBy}
+              />
+              <Dropdown
+                category="Responses"
+                index={5}
+                updateScores={this.updateScores}
+                reviewedBy={this.state.reviewedBy}
+              />
+              <Dropdown
+                category="Industry Knowledge"
+                index={6}
+                updateScores={this.updateScores}
+                reviewedBy={this.state.reviewedBy}
+              />
+              <Dropdown
+                category="Coachability"
+                index={7}
+                updateScores={this.updateScores}
+                reviewedBy={this.state.reviewedBy}
+              />
+              {/* Stand-alone dropbox to select final result*/}
               <select
-                value={this.state.value}
-                onChange={this.handleDropdownChange}
-              >
-                <option value="Select Positivity Score">
-                  Select Positivity Score:
-                </option>
-                <option value={1}>Weak</option>
-                <option value={2}>Average</option>
-                <option value={3}>Strong</option>
-                <option value={4}>Exemplary</option>
-              </select>
-              <select
-                value={this.state.value}
-                onChange={this.handleDropdownChange}
                 name={"finalResult"}
+                value={this.state.value}
+                onChange={this.updateFinalResult}
               >
-                <option value="Select One">Select one:</option>
+                <option value="finalResult">Select Final Result:</option>
                 <option value="Weak">Weak</option>
                 <option value="Average">Average</option>
                 <option value="Strong">Strong</option>
                 <option value="Exemplary">Exemplary</option>
               </select>
+              {/* Submit button */}
               <FormBtn
                 disabled={!this.validateAllValues(this.state)}
                 onClick={this.handleFormSubmit}
