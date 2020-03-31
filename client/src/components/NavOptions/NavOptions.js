f14b7584b28749ba5bdf6157e7fb38e54a6083c1
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2020-03-31 11:38:30 -0400

    fixed manager's view of nav bar

--- client/src/components/NavOptions/NavOptions.js
+++ client/src/components/NavOptions/NavOptions.js
@@ -6,7 +6,7 @@ const NavOptions = props => {
     <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-right">
         <li>
-          {props.userID === "5e664aed0188e521aca69846" ? (
+          {props.userID === "5e83630b5fce490017a3930e" ? (
             <a href={`/assign/${props.userID}`} className="navbar-brand">
               Assign
             </a>
