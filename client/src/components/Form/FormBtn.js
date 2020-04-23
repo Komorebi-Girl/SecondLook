15774a13b072a0af1857dbcff7ca1623638d3db1
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2020-04-23 11:04:59 -0400

    make adjustments to QAform

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-export const FormBtn = ({ children, customStyles }) => (
-  <button style={customStyles} className="btn btn-success">
-    {children}
+// export const FormBtn = ({ children, customStyles }) => (
+//   <button style={customStyles} className="btn btn-success">
+//     {children}
+//   </button>
+// );
+
+export const FormBtn = (props) => (
+  <button {...props} style={props.customStyles} className="btn btn-success">
+    {props.children}
   </button>
 );
