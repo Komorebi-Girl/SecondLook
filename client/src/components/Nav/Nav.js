bbbf000a68c05e6c468fb54d05eaada45b9fe8a5
Komorebi-Girl <stacyjeanphilippe1212@gmail.com>
2020-05-14 09:53:17 -0400

    make tweaks to css of navbar

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -3,39 +3,48 @@ import React from "react";
 const navStyles = {
   backgroundColor: "rgb(50, 198, 230)",
   borderColor: "rgb(50, 198, 230)",
+  padding: "22px 0px",
 };
 
 const navText = {
   fontFamily: "Montserrat",
   fontSize: "2rem",
   color: "rgb(255,255,255)",
-  textAlign: "center",
   textDecoration: "none",
 };
 
 const Nav = (props) => (
   <div className="container-fluid">
     <div className="row" style={navStyles}>
-      <div className="col-xs-12 col-sm-1">
+      <div className="col-xs-12 col-sm-1" style={{ textAlign: "center" }}>
         <a href="/" style={navText}>
           SecondLook
         </a>
       </div>
       {props.userID ? (
         <div>
-          <div className="col-xs-12 col-sm-1 col-sm-push-8 ">
+          <div
+            className="col-xs-12 col-sm-1 col-sm-push-8"
+            style={{ textAlign: "center" }}
+          >
             <a href={`/submit/${props.userID}`} style={navText}>
               Submit
             </a>
           </div>
           {props.userID === "5e83630b5fce490017a3930e" ? (
-            <div className="col-xs-12 col-sm-1 col-sm-push-8">
+            <div
+              className="col-xs-12 col-sm-1 col-sm-push-8"
+              style={{ textAlign: "center" }}
+            >
               <a href={`/assign/${props.userID}`} style={navText}>
                 Assign
               </a>
             </div>
           ) : null}
-          <div className="col-xs-12 col-sm-1 col-sm-push-8">
+          <div
+            className="col-xs-12 col-sm-1 col-sm-push-8"
+            style={{ textAlign: "center" }}
+          >
             <a href="/auth/logout" style={navText}>
               Logout
             </a>
